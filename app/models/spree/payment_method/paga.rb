e646fe38fc80d092ee2c7f1ac1aa5da95767eef3
abhishek <abhishek.jain@vinsol.com>
2013-11-10 00:41:59 +0530

    add attr_accessible to paga method

--- app/models/spree/payment_method/paga.rb
+++ app/models/spree/payment_method/paga.rb
@@ -4,6 +4,8 @@ module Spree
     preference :private_notification_key, :string
     preference :merchant_key, :string
     preference :paga_script, :string
+
+    attr_accessible :preferred_private_notification_key, :preferred_merchant_key, :preferred_paga_script
     def actions
       %w{}
     end
